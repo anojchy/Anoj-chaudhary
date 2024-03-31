35165bfcb29b719370a999cf5ec893c664945851
anojchy <159628391+anojchy@users.noreply.github.com>
2024-03-31 21:52:48 +0545

    Add files via upload
    This is my first program in github to find determinant of 3 x 3 Matrix.

---
+++ determinant.cpp
@@ -0,0 +1,20 @@
+/*  */
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,j,d,a[3][3],b,c,e;
+    printf("Enter the number:\n");
+    for(i=0;i<9;i++){
+    	for(j=0;j<9;j++){
+    		scanf("%d",&a[i][j]);
+    		
+		}
+		b=a[0][0]*(a[1][1]*a[2][2]-a[2][1]*a[1][2]);
+		c=a[0][1]*(a[1][0]*a[2][2]-a[2][0]*a[1][2]);
+		e=a[0][2]*(a[1][0]*a[2][1]-a[2][0]*a[1][1]);
+		printf("The determinant of matrix A is %d .",b-c+e);
+	}
+return 0;
+}
